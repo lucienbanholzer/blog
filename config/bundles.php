a1a2b40e94719bfb6640e0c8b86be8f05e467f3f
Lucien Banholzer <lucien.banholzer@actimage.com>
2018-07-17 18:14:05 +0200

    Add Post entity

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
